2a509f689783b72bb1d22143b17d2d04501fdcdd
Minhpham0203 <Minhpham02032005@gmail.com>
2025-11-02 15:07:39 +0700

    Add local host 5173 to CORS

--- FitForm/settings.py
+++ FitForm/settings.py
@@ -206,7 +206,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://fitness-form.netlify.app"
+    "https://fitness-form.netlify.app",
+    "http://localhost:5173",
     # Thêm domain của frontend khi deploy ở đây
 ]
 # Nếu bạn dùng Cookie (Session Auth) thay vì Token, bật dòng này
